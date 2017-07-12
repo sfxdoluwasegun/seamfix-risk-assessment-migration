be95812f2d892803cf84b4d717e468c1a34a991d
Clement <cessien@seamfix.com>
2017-07-12 12:57:19 +0100

    #NANO-285 modification of RAS algorithm

--- risk-assessment/src/main/java/com/nano/ras/Assessment.java
+++ risk-assessment/src/main/java/com/nano/ras/Assessment.java
@@ -451,6 +451,17 @@ public class Assessment {
 		//return TimeUnit.MILLISECONDS.toDays(rechargeTime.getTime() - subscriberAssessment.getAssessmentInitTime().getTime());
 	}
 	
+	/**
+	 * Checks {@link Subscriber} eligibility for borrowable amount(s) upwards of the threshold amount
+	 * 
+	 * @param Map containing eligibility status and updated {@link SubscriberAssessment}
+	 * @param subscriberAssessment subscribers assessment info
+	 * @param threshold position in the list of borrowableAmounts
+	 * @param subscriberHistories subscribers transactions
+	 * @param subscriber subscriber details
+	 * @return true for highest eligible value
+	 */
+	
 	private Boolean borrowableAmountEligible(Map<String, Object> map, SubscriberAssessment subscriberAssessment, int position, List<SubscriberHistory> subscriberHistories,Subscriber subscriber){
 		Boolean eligible = true;
 			for(int i=(position+1); i<applicationBean.getBorrowableAmounts().size();i++){
@@ -467,6 +478,16 @@ public class Assessment {
 		return eligible;
 	}
 	
+	/**
+	 * Checks {@link Subscriber} eligibility for borrowable amount(s) downwards of the threshold amount
+	 * 
+	 * @param Map containing eligibility status and updated {@link SubscriberAssessment}
+	 * @param subscriberAssessment subscribers assessment info
+	 * @param threshold position in the list of borrowableAmounts
+	 * @param subscriberHistories subscribers transactions
+	 * @param subscriber subscriber details
+	 * @return true/false for highest possible borrowable amount lower than the threshold amount 
+	 */
 	private Boolean borrowableAmountNotEligible(Map<String, Object> map, SubscriberAssessment subscriberAssessment, int position, List<SubscriberHistory> subscriberHistories,Subscriber subscriber){	
 	    Boolean eligible = false;
 		for(int i=(position-1); i>=0; i--){
